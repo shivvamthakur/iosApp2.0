462178c6bda83dcedaef2d8bba2a227a1039d151
Shivam Thakur <shivvamthakurr@gmail.com>
2023-05-15 12:37:21 +0530

    SDK INTEGRATION DONE DATA TRACKING LEFT

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iosApp2.0' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iosApp2.0
+
+end
+pod 'MoEngage-iOS-SDK','~>9.8.0'
--- iosApp2.0/AppDelegate.swift
+++ iosApp2.0/AppDelegate.swift
@@ -4,19 +4,33 @@
 //
 //  Created by Shivam Thakur on 09/05/23.
 //
-
 import UIKit
+import MoEngageSDK
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+   
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
+     
+         //Add your MoEngage App ID
+         var sdkConfig = MoEngageSDKConfig(withAppID: "5ZCYAM3ICF13B6YGB13EPTTM")
+         // Separate initialization methods for Dev and Prod initializations
+         #if DEBUG
+             MoEngage.sharedInstance.initializeDefaultTestInstance(sdkConfig, sdkState: .enabled)
+         #else
+             MoEngage.sharedInstance.initializeDefaultLiveInstance(sdkConfig, sdkState: .enabled)
+         #endif
+    
+     //Rest of the implementation of method
+     //-------
+     return true
+   }
+     
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
